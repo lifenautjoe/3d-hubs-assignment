d17bb4a7f22f84fed3fd860b08a941cbb7b4c4d3
Joel Hernandez <involvmnt@gmail.com>
2016-04-11 01:27:29 +0200

    feat(module: 3dHubsAssignment): declare Promise constant

--- app/main.ts
+++ app/main.ts
@@ -4,6 +4,7 @@ import 'bootstrap-grid-only/css/grid12.css!';
 
 import './3dHubs.module.ts';
 
+import './3dHubs.constants.ts';
 import './3dHubs.values.ts';
 import './3dHubs.config.ts';
 import './3dHubs.run.ts';
